9675f2cef77d07b25753d741f3c4c7b0fd712811
Christof <wowcr@gmx.net>
2017-04-21 12:13:13 +0200

    implemented ggt process pre calc setup (notify all relevant players)

--- ggt.erl
+++ ggt.erl
@@ -1,6 +1,53 @@
 -module(ggt).
 -export([start/1]).
 
-start([ ArbeitsZeit , TermZeit , Quota , GGTProzessnummer , Praktikumsgruppe , Teamnummer , Nameservicenode , Nameservicename , Noordinatorname ]) ->
-  moin
+start([ ArbeitsZeit , TermZeit , Quota , GGTProzessnummer , Starternummer, Praktikumsgruppe , Teamnummer , Nameservicenode , Nameservicename , Koordinatorname ]) ->
+  Nameservice = {Nameservicename, Nameservicenode},
+  GGTName = create_ggt_name(Praktikumsgruppe,Teamnummer,GGTProzessnummer,Starternummer),
+  register(GGTName,self()),
+  notify_nameservice(GGTName,Nameservice),
+  notify_koordinator(GGTName,Nameservice,Koordinatorname),
+  % initialize Mi with 0, will get set via setpm message later
+  calc_ggt_loop(GGTName,Nameservice,0)
   .
+
+create_ggt_name(Praktikumsgruppe,Teamnummer,GGTProzessnummer,Starternummer) ->
+  lists:concat([Praktikumsgruppe,Teamnummer,GGTProzessnummer,Starternummer]).
+
+notify_nameservice(GGTName,Nameservice) ->
+  Nameservice ! {self(),{rebind,GGTName,node()}},
+  receive
+    ok -> io:format("..rebind.done.\n")
+  end.
+
+notify_koordinator(GGTName,Nameservice, Koordinatorname) ->
+  Nameservice ! {self(),{lookup,Koordinatorname}},
+  receive
+    not_found ->
+      io:format("..~p ..not_found.\n",[Koordinatorname]);
+    {pin,{Koordinatorname,Koordinatornode}} ->
+      io:format("...ok: {~p,~p}.\n",[Koordinatorname,Koordinatornode]),
+      Koordinator = {Koordinatorname, Koordinatornode},
+      Koordinator ! {hello,GGTName}
+  end.
+
+calc_ggt_loop(GGTName,Nameservice,Mi) ->
+  receive
+    {setneighbors,LeftN,RightN} ->
+      ;
+    {setpm,MiNeu} ->
+      ;
+    {sendy,Y} ->
+      ;
+    {From,{vote,Initiator}} ->
+      ;
+    {voteYes,Name} ->
+      ;
+    {From,tellmi} ->
+      From ! {mi,Mi},
+      calc_ggt_loop(GGTName,Nameservice,Mi)
+      ;
+    {From,pingGGT} ->
+      From ! {pongGGT,GGTName},
+      calc_ggt_loop(GGTName,Nameservice,Mi)
+end.
